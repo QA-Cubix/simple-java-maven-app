07b3bc6eb917e29bb2085856e6bd6df4f3998218
QA-Cubix <qa.tusharchavan09@gmail.com>
2025-04-07 15:50:15 +0530

    Update2 App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Tushar!";
+    private static final String MESSAGE = "Hello Tushar chavan!";
 
     public App() {}
 
